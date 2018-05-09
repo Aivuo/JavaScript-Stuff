3b8578003e3e8946c938afaac91d82d68b0dc137
Aivuo <rioneda1@gmail.com>
2018-05-09 14:34:31 +0200

    So much.

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,16 @@
 var currentLVL = 0;
 var walls = [];
 var blocks = [];
+var goals = [];
+var blocks2 = [];
 var player;
+
+var collideLeft = false;
+var collideRight = false;
+var collideUp = false;
+var collideDown = false;
+
+var ctx;
 
 var gameArea = {
     canvas: document.createElement("canvas"),
@@ -15,27 +24,27 @@ var gameArea = {
 
 
         //Går igenom hela arrayen och skriver ut allt i loggen.
-        for (let index = 0; index < mapsArray[currentLVL].mapGrid.length; index++) {
-            for (let index2 = 0; index2 < mapsArray[currentLVL].mapGrid[index].length; index2++) {
+        for (index = 0; index < mapsArray[currentLVL].mapGrid.length; index++) {
+            for (index2 = 0; index2 < mapsArray[currentLVL].mapGrid[index].length; index2++) {
 
                 switch (mapsArray[currentLVL].mapGrid[index][index2].toString()) {
                     case "W":
-                        walls.push(new wallBlock(30, 30, "red", index2 * 30, index * 30));
-                        console.log("Woop");
+                        blocks2.push(new allBlocks(30, 30, "red", index2 * 30, index * 30, index2, index, "wall"));
+                        //console.log("Woop");
                         break;
                     case "G":
-                        walls.push(new wallBlock(30, 30, "yellow", index2 * 30, index * 30));
-                        console.log("Goop");
+                        blocks2.push(new allBlocks(30, 30, "yellow", index2 * 30, index * 30, index2, index, "goal"));
+                        //console.log("Goop");
                         break;
 
                     case "B":
-                        blocks.push(new moveBlock(30, 30, "green", index2 * 30, index * 30));
-                        console.log("Bloop");
+                        blocks2.push(new allBlocks(30, 30, "green", index2 * 30, index * 30, index2, index, "move"));
+                        //console.log("Bloop");
                         break;
 
                     case "P":
-                        player = new playerBlock(30, 30, "blue", index2 * 30, index * 30);
-                        console.log("Ploop");
+                        player = new playerBlock(30, 30, "blue", index2 * 30, index * 30, index2, index);
+                        //console.log("Ploop");
                         break;
 
                     default:
@@ -46,7 +55,7 @@ var gameArea = {
             /* for (const key in mapsArray[currentLVL].mapGrid[index]) {
                 
             } */
-            console.log("Switch!");
+            //console.log("Switch!");
         }
 
         window.addEventListener("keydown", function (e) {
@@ -59,66 +68,125 @@ var gameArea = {
     }
 }
 
-function wallBlock(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-    this.update = function () {
-        ctx = gameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+class allBlocks {
+    constructor(width, height, color, x, y, indexX, indexY, type) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.positionX = indexX;
+        this.positionY = indexY;
+        // console.log(this.positionX);
+        // console.log(this.positionY);
+        this.update = function () {
+            this.move();
+            ctx = gameArea.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        };
+        this.move = function () {
+            if (this.type == "move") {
+                if (this.positionX == player.positionX && this.positionY == player.positionY) {
+                    if (this.positionX < player.formerPositionX && this.positionY == player.formerPositionY) {
+                        this.x -= 30;
+                        this.positionX -= 1;
+                    }
+                    if (this.positionX > player.formerPositionX && this.positionY == player.formerPositionY) {
+                        this.x += 30;
+                        this.positionX += 1;
+                    }
+                    if (this.positionY < player.formerPositionY && this.positionX == player.formerPositionX) {
+                        this.y -= 30;
+                        this.positionY -= 1;
+                    }
+                    if (this.positionY > player.formerPositionY && this.positionX == player.formerPositionX) {
+                        this.y += 30;
+                        this.positionY += 1;
+                    }
+                }
+            }
+        };
     }
 }
 
-function moveBlock(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-    this.update = function () {
-        ctx = gameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+class playerBlock {
+    constructor(width, height, color, x, y, indexX, indexY) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.formerPositionX = indexX;
+        this.formerPositionY = indexY;
+        this.positionX = this.formerPositionX;
+        this.positionY = this.formerPositionY;
+        this.x = x;
+        this.y = y;
+        this.colliding = function (intersectingObject) {
+            var myX = this.positionX;
+            var myY = this.positionY;
+            var otherX = intersectingObject.positionX;
+            var otherY = intersectingObject.positionY;
+            
+            if (intersectingObject.type == "wall") {
+                if (otherX == myX - 1 && otherY == myY) {
+                    collideLeft = true;
+                }
+                if (otherX == myX + 1 && otherY == myY) {
+                    collideRight = true;
+                }
+                if (otherY == myY - 1 && otherX == myX) {
+                    collideUp = true;
+                }
+                if (otherY == myY + 1 && otherX == myX) {
+                    collideDown = true;
+                }
+            }
+        };
+
+        this.move = function () {
+            if ((gameArea.key && gameArea.key == 65 || gameArea.key && gameArea.key == 37) && collideLeft == false) {
+                player.speedX = -30;
+                player.formerPositionX = player.positionX;
+                player.formerPositionY = player.positionY;
+                player.positionX -= 1;
+            }
+            if ((gameArea.key && gameArea.key == 68 || gameArea.key && gameArea.key == 39) && collideRight == false) {
+                player.speedX = 30;
+                player.formerPositionX = player.positionX;
+                player.formerPositionY = player.positionY;
+                player.positionX += 1;
+            }
+            if ((gameArea.key && gameArea.key == 87 || gameArea.key && gameArea.key == 38) && collideUp == false) {
+                player.speedY = -30;
+                player.formerPositionY = player.positionY;
+                player.formerPositionX = player.positionX;
+                player.positionY -= 1;
+            }
+            if ((gameArea.key && gameArea.key == 83 || gameArea.key && gameArea.key == 40) && collideDown == false) {
+                player.speedY = 30;
+                player.formerPositionY = player.positionY;
+                player.formerPositionX = player.positionX;
+                player.positionY += 1;
+            }
+            collideLeft = false;
+            collideRight = false;
+            collideUp = false;
+            collideDown = false;
+        };
+        this.update = function () {
+            ctx = gameArea.context;
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        };
+        this.newPos = function () {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            gameArea.key = false;
+        };
     }
-}
-
-function playerBlock(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-
-    this.move = function (){
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = intersectingObject.x;
-        var otherright = intersectingObject.x + (intersectingObject.width);
-        var othertop = intersectingObject.y;
-        var otherbottom = intersectingObject.y + (intersectingObject.height);
-
-
-    }
-
-    this.update = function () {
-        ctx = gameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function () {
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        gameArea.key = false;
-    }
-
 }
 
 function startGame() {
@@ -129,17 +197,18 @@ function updateGameArea() {
     gameArea.clear();
     player.speedX = 0;
     player.speedY = 0;
-    for (i = 0; i < walls.length; i++) {
-        walls[i].update();
+    for (i = 0; i < blocks2.length; i++) {
+        blocks2[i].update();
+        player.colliding(blocks2[i]);
     }
-    for (i = 0; i < blocks.length; i++) {
-        blocks[i].update();
-    }
-    if (gameArea.key && gameArea.key == 65 || gameArea.key && gameArea.key == 37) { player.speedX = -30; }
-    if (gameArea.key && gameArea.key == 68 || gameArea.key && gameArea.key == 39) { player.speedX = 30; }
-    if (gameArea.key && gameArea.key == 87 || gameArea.key && gameArea.key == 38) { player.speedY = -30; }
-    if (gameArea.key && gameArea.key == 83 || gameArea.key && gameArea.key == 40) { player.speedY = 30; }
-    
+
+    // for (i = 0; i < blocks.length; i++) {
+    //     blocks[i].update();
+    // }
+    // for (i = 0; i < goals.length; i++) {
+    //     goals[i].update();
+    // }
+    player.move();
     player.newPos();
     player.update();
 
